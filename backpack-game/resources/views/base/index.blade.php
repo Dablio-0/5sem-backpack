43d13f5900f4f72e142b346526d15f0b364bbb88
Willians Henrique <willianshenrique94@gmail.com>
2025-04-06 16:50:09 -0300

    modification: added matrix display box

--- backpack-game/resources/views/base/index.blade.php
+++ backpack-game/resources/views/base/index.blade.php
@@ -1,39 +1,40 @@
 @extends('layout')
 
 @section('content')
-    <h1>Base</h1>
-    <p>Welcome to our base page!</p>
+    <h1>Metódos Genéricos</h1>
+    <p>Configuração Fixa !</p>
 
     <div class="row">
-    <div class="col s12">
-        <div class="card">
-            <div class="card-content">
-                <span class="card-title">{{ __('Filtros') }}</span>
-                <form method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="input-field col s12 l3 xl2">
-                            <input type="text" name="age" id="age" value="{{ $age ?? '' }}" />
-                            <label for="age">{{ __('Idade') }}</label>
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Problema da Mochila</span>
+                    <form method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="input-field col s12 l3 xl2">
+                                <input type="text" name="capacity" id="capacity" value="{{ $capacity ?? '' }}" />
+                                <label for="capacity">Capacidade da Mochila</label>
+                            </div>
+                            <div class="input-field col s12 l3 xl2">
+                                <input type="text" name="item_count" id="item_count" value="{{ $item_count ?? '' }}" />
+                                <label for="item_count">Quantidade de Itens</label>
+                            </div>
                         </div>
-                        <div class="input-field col s12 l3 xl2">
-                            <input type="text" name="name" id="name" value="{{ $name ?? '' }}" />
-                            <label for="name">{{ __('Nome') }}</label>
+                        <div>
+                            <input type="submit" value="Gerar Problema" class="btn waves-effect waves-light" />
                         </div>
-                        <div class="input-field col s12 l3 xl2">
-                            <input type="text" name="city" id="city" value="{{ $city ?? '' }}"/>
-                            <label for="city">{{ __('Cidade') }}</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input type="text" name="gender" id="gender" value="Male" />
-                            <label for="gender">{{ __('Gênero') }}</label>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Visualização dos Dados</span>
+                    array de dados
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
